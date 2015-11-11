db18067c66caac8c9c705819e9687e38935778ce
Aldin Kiselica <aldin.kiselica.94@gmail.com>
2015-11-11 14:11:40 +0100

    webapp

---
+++ WebApp/index.php
@@ -0,0 +1,18 @@
+<head>
+    
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="css/main.css">
+
+</head>
+
+<body>
+    <?php 
+        include 'partials/login.php'
+    ?>
+    
+    
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+</body>
