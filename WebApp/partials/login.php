fed3f9c7f2e18b5182175ace8b9f44d89068e5f9
Aldin Kiselica <aldin.kiselica.94@gmail.com>
2015-11-16 14:08:20 +0100

    upgrade part1

--- WebApp/partials/login.php
+++ WebApp/partials/login.php
@@ -10,7 +10,7 @@
                 <div style="float:right; font-size: 77%; position: relative; top:-10px"><a href="#">Zaboravljena lozinka?</a></div>
             </div>     
 
-            <div style="padding-top:20px" class="panel-body" >
+            <div style="padding-top:30px" class="panel-body" >
                 <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
                     
                 <form id="loginform" class="form-horizontal" role="form">
@@ -20,12 +20,12 @@
                         <input id="login-username" type="text" class="form-control" name="username" value="" placeholder="Korisnik">                                        
                     </div>
                         
-                    <div style="margin-bottom: 15px" class="input-group">
+                    <div style="margin-bottom: 25px" class="input-group">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
                         <input id="login-password" type="password" class="form-control" name="password" placeholder="Lozinka">
                     </div>
 
-                        <div style="margin-top:40px" class="form-group">
+                        <div style="margin-top:30px" class="form-group">
                             <!-- Button -->
 
                             <div class="col-sm-12 controls">
