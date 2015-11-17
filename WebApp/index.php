57241952025b9ccce41b6f328f7d66943954515a
edenasevic1 <edenasevic1@etf.unsa.ba>
2015-11-17 14:27:45 +0100

    index change

--- WebApp/index.php
+++ WebApp/index.php
@@ -7,7 +7,6 @@
     	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular-route.min.js"></script>
     	<script src="https://cdn.firebase.com/js/client/2.0.4/firebase.js"></script>
     	<script src="https://cdn.firebase.com/libs/angularfire/0.9.0/angularfire.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	    <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
 	    <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.css">
 	    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
@@ -16,6 +15,9 @@
 	</head>
 
 	<body>
+		<?php 
+	    	include "partials/glavni-panel.php";
+	    ?>
 		<h1>Kadrovska služba</h2>
 		<h3><a href="#/allUsers">Korisnici</a></h3>
 		<h3><a href="#/allThesis">Zavrsni radovi</a></h3>
@@ -35,8 +37,8 @@
 	    <!-- LOAD ANGULAR ROUTES -->
 	    <script src="app/routes.js"></script>
 
-
-	    <script src="bootstrap/js/bootstrap.min.js"></script>
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	    <!--script src="bootstrap/js/bootstrap.min.js"></script-->
 	    <script src="bootstrap/js/bootstrap.js"></script>
 	</body>
 </html>
