251a42618403f850bcc45f23be697ae832d2a3c9
edenasevic1 <edenasevic1@etf.unsa.ba>
2015-11-17 17:10:51 +0100

    Testiranje proslijedjivanja podataka

--- WebApp/app/controllers/thesisController.js
+++ WebApp/app/controllers/thesisController.js
@@ -1,9 +1,16 @@
 'use strict'
 kadrovska.controller('getAllThesisCtrl', function ($scope, thesisService) {
+    
     $scope.thesis = [];
+    
     thesisService.getThesis().then(function (results) {
         $scope.thesis = results.data;
     }, function (error) {
         alert(error.data.message);
     });
+
+    $scope.proslijediZavrsne = function(zavrsniRadovi){
+    	console.log(zavrsniRadovi);
+    }
+
 });
