dbbaf2efcca9d448c8a5f16aa9f741a7c90f25e0
Aldin Kiselica <aldin.kiselica.94@gmail.com>
2015-11-18 12:05:18 +0100

    minifix

--- WebApp/partials/glavni-panel.php
+++ WebApp/partials/glavni-panel.php
@@ -53,8 +53,25 @@
     
         <div class="panel-box">
             <div class="panel-obj1">
+                <div class="btn-group-vertical" role="group" aria-label="...">
+  <button type="button" class="btn btn-default">1</button>
+  <button type="button" class="btn btn-default">2</button>
+
+  <div class="btn-group" role="group">
+    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      Dropdown
+      <span class="caret"></span>
+    </button>
+    <ul class="dropdown-menu">
+      <li><a href="#">Dropdown link</a></li>
+      <li><a href="#">Dropdown link</a></li>
+    </ul>
+  </div>
+</div>
             </div>
             <div class="panel-obj2">
                 <div ng-view=""></div>
+                
+                <button type="submit" class="btn btn-primary btn-custom">Pošalji</button>
             </div>
         </div>
