79d873911ed7dd07ab8d4bd3f8017c4e105c0277
farisodobasic <fodobasic1@etf.unsa.ba>
2015-12-02 11:53:46 +0100

    dump

--- Dokumentacija/Dump baze/kadrovska.sql
+++ Dokumentacija/Dump baze/kadrovska.sql
@@ -1,3 +1,5 @@
+-- MySQL Workbench Forward Engineering
+
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
@@ -5,6 +7,10 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema kadrovska
+-- -----------------------------------------------------
+
 -- -----------------------------------------------------
 -- Schema kadrovska
 -- -----------------------------------------------------
@@ -19,6 +25,7 @@ USE `kadrovska` ;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `kadrovska`.`TipDokumenta` (
   `id` INT NOT NULL AUTO_INCREMENT,
+  `Opis` VARCHAR(45) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
@@ -29,6 +36,9 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `kadrovska`.`Dokument` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `TipDokumenta_id` INT NOT NULL,
+  `Naslov` VARCHAR(45) NULL,
+  `Tekst` LONGTEXT NULL,
+  `Datum kreiranja` DATETIME NULL,
   PRIMARY KEY (`id`, `TipDokumenta_id`),
   INDEX `fk_Dokument_TipDokumenta1_idx` (`TipDokumenta_id` ASC),
   CONSTRAINT `fk_Dokument_TipDokumenta1`
@@ -40,11 +50,51 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
+-- Table `kadrovska`.`Pozicija`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `kadrovska`.`Pozicija` (
+  `id` INT NOT NULL,
+  `Opis` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `kadrovska`.`Odsjek`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `kadrovska`.`Odsjek` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `Naziv` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `kadrovska`.`Osoba`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `kadrovska`.`Osoba` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`))
+  `Pozicija_id` INT NOT NULL,
+  `Ime` VARCHAR(45) NULL,
+  `Prezime` VARCHAR(45) NULL,
+  `JMBG` VARCHAR(45) NULL,
+  `Adresa` VARCHAR(45) NULL,
+  `Email` VARCHAR(45) NULL,
+  `Telefon` VARCHAR(45) NULL,
+  `Odsjek_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `Pozicija_id`, `Odsjek_id`),
+  INDEX `fk_Osoba_Pozicija1_idx` (`Pozicija_id` ASC),
+  INDEX `fk_Osoba_Odsjek1_idx` (`Odsjek_id` ASC),
+  CONSTRAINT `fk_Osoba_Pozicija1`
+    FOREIGN KEY (`Pozicija_id`)
+    REFERENCES `kadrovska`.`Pozicija` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Osoba_Odsjek1`
+    FOREIGN KEY (`Odsjek_id`)
+    REFERENCES `kadrovska`.`Odsjek` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -53,6 +103,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `kadrovska`.`Status` (
   `id` INT NOT NULL AUTO_INCREMENT,
+  `Opis` VARCHAR(45) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
@@ -62,6 +113,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `kadrovska`.`Privilegije` (
   `id` INT NOT NULL AUTO_INCREMENT,
+  `Opis` VARCHAR(45) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
